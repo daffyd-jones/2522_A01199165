916ba9647aa8234ab17fddf954afa514a0fa246d
daffyd-jones <daffydbjones@gmail.com>
2020-11-16 18:19:24 -0800

    Lab05

--- src/ca/bcit/comp2522/labs/lab05/SpyCar.java
+++ src/ca/bcit/comp2522/labs/lab05/SpyCar.java
@@ -2,9 +2,14 @@ package ca.bcit.comp2522.labs.lab05;
 
 public class SpyCar extends Submarine {
 
+  private int mileage;
+
   public SpyCar(final String make, final String model, final int numPassengers,
-                final int topSpeed, final int maxDepth) {
+                final int topSpeed, final int maxDepth, final int  mileage) {
     super(make, model, numPassengers, topSpeed, maxDepth);
+    this.mileage = mileage;
   }
+
+
 
 }
--- src/ca/bcit/comp2522/labs/lab05/Tester.java
+++ src/ca/bcit/comp2522/labs/lab05/Tester.java
@@ -9,14 +9,14 @@ public class Tester {
     public Tester() {
         Garage mine = new Garage();
 
-        mine.add(new Car("Porsche","911",2,300,15));
-        mine.add(new Car("Ferrari","GTO",2,301,12));
-        mine.add(new Car("Citroen","2CV",5,95,40));
-        mine.add(new Submarine("Bayliner","Nemo",12,20,150));
-        mine.add(new Motorcycle("Ducati","900SS",1,200,35,90));
-        mine.add(new Motorcycle("Yamaha","Roadstar",2,150,55,120));
-        mine.add(new Truck("VW","Microbus",2000,6,130,45));
-        mine.add(new SpyCar("Lotus", "Espirit", 2, 222, 1063));
+        mine.add(new Car("Porsche", "911", 2, 300, 15));
+        mine.add(new Car("Ferrari", "GTO", 2, 301, 12));
+        mine.add(new Car("Citroen", "2CV", 5, 95, 40));
+        mine.add(new Submarine("Bayliner", "Nemo", 12, 20, 150));
+        mine.add(new Motorcycle("Ducati", "900SS", 1, 200, 35, 90));
+        mine.add(new Motorcycle("Yamaha", "Roadstar", 2, 150, 55, 120));
+        mine.add(new Truck("VW", "Microbus", 2000, 6, 130, 45));
+        mine.add(new SpyCar("Lotus", "Espirit", 2, 222, 1063, 12));
 
         mine.report();
     }
